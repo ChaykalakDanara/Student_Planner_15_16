7d7811b25c584f8dc376806d0143adad85338e8f
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-04-27 20:57:33 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/chaykalak/studentplanner/MainActivity.kt
+++ app/src/main/java/com/chaykalak/studentplanner/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.chaykalak.studentplanner.navigation.StudentPlannerNavHost
 import com.chaykalak.studentplanner.ui.theme.StudentPlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +21,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudentPlannerTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudentPlannerNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
